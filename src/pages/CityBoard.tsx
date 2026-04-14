014dc2e9002069cc62e7f78e596d636fde50b355
R4G3RUNN3R <ragerunner__gr@hotmail.com>
2026-04-14 02:55:34 -0700

    Replace active City and City Board pages with structured versions

--- src/pages/CityBoard.tsx
+++ src/pages/CityBoard.tsx
@@ -1,5 +1,13 @@
-import { PlaceholderPage } from "../components/layout/PlaceholderPage";
+import { AppShell } from "../components/layout/AppShell";
+import CityBoardSections from "../components/city/CityBoardSections";
 
 export default function CityBoardPage() {
-  return <PlaceholderPage title="City Board" />;
+  return (
+    <AppShell
+      title="City Board"
+      hint="Nexis public board rebuilt as the city newspaper equivalent: jobs, notices, opportunities, bounties, and property leads."
+    >
+      <CityBoardSections />
+    </AppShell>
+  );
 }
